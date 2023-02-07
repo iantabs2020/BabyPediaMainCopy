319ced5a3d79006d05d4e15e6d3efe6228bc0071
Ian <Ian@DESKTOP-PBR1QI2>
2023-02-07 18:36:03 +0800

    Add project files.

---
+++ BabyPedia/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server;
+using BabyPedia.Models;
+using System.IO;
+using System.Web;
+
+namespace BabyPedia.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Home()
+    {
+        return View();
+    }
+
+    public IActionResult Services()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+    public IActionResult ContactUs()
+    {
+        return View();
+    }
+    public IActionResult Register()
+    {
+        return View();
+    }
+    public IActionResult Login()
+    {
+        return View();
+    }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ BabyPedia/Controllers/RegistrationController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BabyPedia.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult ParentRegistration()
+        {
+            return View();
+        }
+
+        public IActionResult PediaRegistration()
+        {
+            return View();
+        }
+
+        public IActionResult DataPrivacy()
+        {
+            return View();
+        }
+
+    }
+}
