baa583dc2e7dde51cef263af2f8ba0c120c4d2bd
Ian <julian.tablante@benilde.edu.ph>
2023-02-08 17:24:27 +0800

    update ulit

---
+++ BabyPedia/Controllers/RecordsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BabyPedia.Controllers
+{
+    public class RecordsController : Controller
+    {
+        public IActionResult RecordPatient()
+        {
+            return View();
+        }
+    }
+}
---
+++ BabyPedia/Controllers/VaccineInfoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BabyPedia.Controllers
+{
+    public class VaccineInfoController : Controller
+    {
+        public IActionResult HepA()
+        {
+            return View();
+        }
+
+        public IActionResult BCG()
+        {
+            return View();
+        }
+
+        public IActionResult MMR()
+        {
+            return View();
+        }
+
+        public IActionResult IPV()
+        {
+            return View();
+        }
+
+
+    }
+}
