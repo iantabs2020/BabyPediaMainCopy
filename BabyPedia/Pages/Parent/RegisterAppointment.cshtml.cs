635fbea5c1c94fc160650f1255f2a5e81599ae99
reneechynnagahon <46592737+reneechynnagahon@users.noreply.github.com>
2023-02-15 17:28:01 +0800

    Add files via upload

---
+++ BabyPedia/Pages/Parent/RegisterAppointment.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BabyPedia.Pages.Parent;
+
+public class RegisterAppointment : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
