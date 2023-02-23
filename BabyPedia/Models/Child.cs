7c55202040c8882e26a07b82f1357f593547b1fe
iantabs2020 <59952422+iantabs2020@users.noreply.github.com>
2023-02-23 08:03:16 +0800

    Update Child.cs
    Code Update Child

--- BabyPedia/Models/Child.cs
+++ BabyPedia/Models/Child.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace BabyPedia.Models;
 
-public class Child : IdentityUser
+public class Child
 {
+    public long Id { get; set; }
+
+    public string? Username => $"{FirstName} {LastName}";
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
     public string? PlaceOfBirth { get; set; }
 
     public double? BirthHeight { get; set; }
@@ -12,11 +20,13 @@ public class Child : IdentityUser
 
     public string? Sex { get; set; }
 
-    public DateTime DateTimeCreated { get; set; } = DateTime.Now;
+    [DataType(DataType.Date)] public DateTime DateTimeCreated { get; set; } = DateTime.Now;
 
-    public ImmunizationRecord? ImmunizationRecord { get; set; }
+    public List<ImmunizationRecord> ImmunizationRecord { get; set; } =
+        new List<ImmunizationRecord>();
 
     public Parent Parent { get; set; }
 
-    public List<Appointment> Appointments { get; set; } = new List<Appointment>();
+    public List<Appointment> Appointments { get; set; } =
+        new List<Appointment>();
 }
