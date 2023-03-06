a0d06799ae3507348e945f13f00e9279acab10ba
Johannes Palor <31757385+JohannesPalor@users.noreply.github.com>
2023-03-06 18:29:38 +0800

    Pedia Paypal Payment

---
+++ BabyPedia/Models/PediaPayment.cs
@@ -0,0 +1,12 @@
+﻿namespace BabyPedia.Models;
+
+public class PediaPayment
+{
+    public long Id { get; set; }
+
+    public double Payment { get; set; }
+
+    public PartneredPedia? PartneredPedia { get; set; }
+
+    public DateTime DatePaid { get; set; } = DateTime.Now;
+}
