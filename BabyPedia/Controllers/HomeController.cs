32a13bc79ee25180f247677c1321081f46722a71
Johannes Palor <31757385+JohannesPalor@users.noreply.github.com>
2023-03-06 18:33:39 +0800

    Update HomeController.cs

--- BabyPedia/Controllers/HomeController.cs
+++ BabyPedia/Controllers/HomeController.cs
@@ -4,27 +4,36 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Server;
 using BabyPedia.Models;
 using System.IO;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Web;
 using BabyPedia.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
 
 namespace BabyPedia.Controllers;
 
 public class HomeController : Controller
 {
+    private static Random _random = new Random();
     private readonly ILogger<HomeController> _logger;
     private readonly SignInManager<IdentityUser> _signInManager;
     private readonly UserManager<IdentityUser> _userManager;
     private readonly BabyPediaContext _babyPediaContext;
+    private readonly EmailHandler _emailHandler;
+    private readonly IServerAddressesFeature _addressesFeature;
 
     public HomeController(ILogger<HomeController> logger, SignInManager<IdentityUser> signInManager,
-        UserManager<IdentityUser> userManager, BabyPediaContext babyPediaContext)
+        UserManager<IdentityUser> userManager, BabyPediaContext babyPediaContext, EmailHandler emailHandler)
     {
         _logger = logger;
         _signInManager = signInManager;
         _userManager = userManager;
         _babyPediaContext = babyPediaContext;
+        _emailHandler = emailHandler;
     }
 
     public IActionResult Home()
@@ -57,6 +66,134 @@ public class HomeController : Controller
         return View();
     }
 
+    public async Task<string> GetAccessToken()
+    {
+        HttpClient client = new HttpClient();
+        client.BaseAddress = new Uri("https://api.sandbox.paypal.com");
+
+        string clientId = "AbhPFeMhKFkyF6Xv-9ft8iO5HPmYcYTLA8QeZXmfA5Yqwl8MDbJtrpsOKaLT37Le0tTzR9ssUM1uLxqD";
+        string secret = "EEVR-vvxoS1HFWEoQ-Ub9jg9e_nT-QCGCPARxt28NvDfCT-8awWyyse5ARIp-cb8IIni-ZZY-yfBRDCc";
+
+        var authBytes = Encoding.UTF8.GetBytes($"{clientId}:{secret}");
+        var authString = Convert.ToBase64String(authBytes);
+
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authString);
+
+        var body = new StringContent("grant_type=client_credentials", Encoding.UTF8,
+            "application/x-www-form-urlencoded");
+
+        var response = await client.PostAsync("/v1/oauth2/token", body);
+
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        return JObject.Parse(responseString)["access_token"].ToString();
+    }
+
+    [HttpPost]
+    [Route("api/verifyPayment")]
+    public async Task<IActionResult> VerifyPayment([FromBody] dynamic paymentDetails, [FromQuery] bool isPedia = true)
+    {
+        dynamic payment = JObject.Parse(paymentDetails.ToString());
+
+        string accessToken = await GetAccessToken();
+
+        _logger.LogInformation("Payment received!");
+        HttpClient client = new HttpClient();
+        client.BaseAddress = new Uri("https://api.sandbox.paypal.com");
+
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        var response = await client.GetAsync($"/v2/checkout/orders/{payment.id}");
+
+        if (response.IsSuccessStatusCode)
+        {
+            var paymentAmount = Double.Parse(payment.purchase_units[0].amount.value.ToString());
+            if (isPedia)
+            {
+                var currentUser = await _userManager.GetUserAsync(User);
+                var currentPedia =
+                    await _babyPediaContext.PartneredPedias.FirstOrDefaultAsync(x => x.Id == currentUser.Id);
+
+                var pediaPayment = new PediaPayment()
+                {
+                    Payment = paymentAmount,
+                    PartneredPedia = currentPedia
+                };
+
+                await _babyPediaContext.PediaPayments.AddAsync(pediaPayment);
+                await _babyPediaContext.SaveChangesAsync();
+            }
+            else
+            {
+                var currentUser = await _userManager.GetUserAsync(User);
+                var currentParent = await _babyPediaContext.Parents.FirstOrDefaultAsync(x => x.Id == currentUser.Id);
+
+                var pediaPayment = new AppointmentPayment()
+                {
+                    Amount = paymentAmount,
+                    Parent = currentParent,
+                    Status = string.Empty
+                };
+
+                await _babyPediaContext.AppointmentPayments.AddAsync(pediaPayment);
+                await _babyPediaContext.SaveChangesAsync();
+            }
+
+            // Payment verification succeeded!
+            return Redirect("/pedia/subscriptionpayment");
+        }
+        else
+        {
+            // Payment verification failed!
+            return BadRequest();
+        }
+    }
+
+
+    private static Dictionary<int, string> shortenedTokens =
+        new Dictionary<int, string>();
+
+    // testing only
+    [HttpGet("/test/sendemail")]
+    public async Task<IActionResult> SendEmailTest()
+    {
+        return Content($"{HttpContext.Connection.LocalPort}");
+    }
+
+    [HttpGet("/email/{id}")]
+    public async Task<IActionResult> SendEmailConfirmation(string id)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+        var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+        var shortenedCode = _random.Next(10000, 99999);
+
+        shortenedTokens.Add(shortenedCode, token);
+
+        await _emailHandler.SendEmail(user.Email, "BabyPedia Verification",
+            @$"Please verify your account http://localhost:{HttpContext.Connection.LocalPort}/emailconfirm/{id}?token={shortenedCode}");
+
+        return Redirect("/home/login?info=Please check your email for verification!");
+    }
+
+    [HttpGet("/emailconfirm/{id}")]
+    public async Task<IActionResult> ConfirmEmail(string id, [FromQuery] int token)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+
+        if (user.EmailConfirmed)
+            return Redirect("/home/login?info=Already Verified!");
+
+        string realToken = string.Empty;
+        if (shortenedTokens.TryGetValue(token, out var sToken))
+        {
+            realToken = sToken;
+        }
+
+        var confirmationResult = await _userManager.ConfirmEmailAsync(user, realToken);
+
+        return Redirect("/home/login?info=Verified!");
+    }
+
+    [Authorize(Roles = "Parent")]
     [HttpGet("/parent/currentappointment")]
     public async Task<IActionResult> ParentCurrentAppointment()
     {
@@ -72,21 +209,22 @@ public class HomeController : Controller
             .ThenInclude(x => x.Payment)
             .FirstOrDefaultAsync(x => x.Id == currentUserId);
         return View(currentParent.Appointments.LastOrDefault());
-
     }
 
+    [Authorize(Roles = "Pedia")]
     [HttpGet("/pedia/appointment/{id}")]
     public async Task<IActionResult> PediaChat(long id)
     {
         return View(await _babyPediaContext.Appointments
-            .Include(x=>x.Child)
-            .Include(x=>x.Parent)
-            .Include(x=>x.Payment)
-            .Include(x=>x.Pedia)
-            .Include(x=>x.AppointmentType)
+            .Include(x => x.Child)
+            .Include(x => x.Parent)
+            .Include(x => x.Payment)
+            .Include(x => x.Pedia)
+            .Include(x => x.AppointmentType)
             .FirstOrDefaultAsync(x => x.Id == id));
     }
 
+    [Authorize(Roles = "Pedia")]
     [HttpGet("/pedia/childinformation/{id}")]
     public async Task<IActionResult> ChildInformation(long id)
     {
@@ -94,20 +232,23 @@ public class HomeController : Controller
     }
 
 
+    [Authorize(Roles = "Pedia")]
     [HttpGet("/pedia/immunizationrecord/{id}")]
     public async Task<IActionResult> immunizationrecord(long id)
     {
         return View(await _babyPediaContext.Children.Include(x => x.ImmunizationRecord)
             .FirstOrDefaultAsync(x => x.Id == id));
     }
-    
 
+
+    [Authorize(Roles = "Admin")]
     [HttpGet("/verify/{id}")]
     public async Task<IActionResult> Verification(string id)
     {
         return View(await _babyPediaContext.PartneredPedias.FirstOrDefaultAsync(x => x.Id == id));
     }
 
+    [Authorize(Roles = "Admin")]
     [HttpGet("/user/delete/{id}")]
     public async Task<IActionResult> DeleteUser(string id)
     {
@@ -117,6 +258,7 @@ public class HomeController : Controller
         return Redirect($"/admin?message={user.UserName} has been deleted!");
     }
 
+    [Authorize(Roles = "Admin")]
     [HttpGet("/confirm/{id}")]
     public async Task<IActionResult> ConfirmPedia(string id)
     {
@@ -130,28 +272,32 @@ public class HomeController : Controller
         );
     }
 
+    [Authorize(Roles = "Parent")]
     [HttpGet("/prescriptions/{id}")]
     public async Task<IActionResult> Prescription(long id)
     {
         return View(await _babyPediaContext.Appointments
-            .Include(x=>x.Child)
-            .Include(x=>x.Parent)
-            .Include(x=>x.Payment)
-            .Include(x=>x.AppointmentType)
+            .Include(x => x.Child)
+            .Include(x => x.Parent)
+            .Include(x => x.Pedia)
+            .Include(x => x.Payment)
+            .Include(x => x.AppointmentType)
             .FirstOrDefaultAsync(x => x.Id == id));
     }
-    
+
+    [Authorize(Roles = "Pedia")]
     [HttpGet("/prescribe/{id}")]
     public async Task<IActionResult> PrescribeForm(long id)
     {
         return View(await _babyPediaContext.Appointments
-            .Include(x=>x.Child)
-            .Include(x=>x.Parent)
-            .Include(x=>x.Payment)
-            .Include(x=>x.AppointmentType)
+            .Include(x => x.Child)
+            .Include(x => x.Parent)
+            .Include(x => x.Payment)
+            .Include(x => x.AppointmentType)
             .FirstOrDefaultAsync(x => x.Id == id));
     }
 
+    [Authorize(Roles = "Pedia")]
     [HttpPost("/prescribe/add")]
     public async Task<IActionResult> AddPrescription(Appointment appointment)
     {
@@ -160,15 +306,18 @@ public class HomeController : Controller
 
         _babyPediaContext.Update(exisintAppointment);
         await _babyPediaContext.SaveChangesAsync();
-        
+
         return Redirect("/pedia/appointmentlist");
     }
 
+
+    [Authorize(Roles = "Pedia,Admin")]
     [HttpGet("/vaccines/{id}")]
     public async Task<IActionResult> VaccineInformation(long id)
     {
         return View(await _babyPediaContext.Vaccines.FirstOrDefaultAsync(x => x.Id == id));
     }
+
 
     [HttpGet("/logout")]
     public async Task<IActionResult> LogoutUser()
@@ -200,6 +349,11 @@ public class HomeController : Controller
                 return Redirect("/admin/");
             }
         }
+
+        var existingUser = await _userManager.FindByNameAsync(username);
+        if (!existingUser.EmailConfirmed)
+            return Redirect(
+                $"/home/login?error=Please confirm your email!");
 
         return Redirect(
             $"/home/login?error=Bad credentials!");
